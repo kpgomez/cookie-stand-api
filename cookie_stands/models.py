7fe3c61d6e46221e572e1f8dd871f26cd0034c98
Kiengchay Gomez <kphoutthavong@gmail.com>
2024-02-04 02:26:57 -0600

    add save method

--- cookie_stands/models.py
+++ cookie_stands/models.py
@@ -1,3 +1,5 @@
+import random
+
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
@@ -21,3 +23,18 @@ class CookieStand(models.Model):
     def get_absolute_url(self):
         return reverse('cookie_stand_detail', args=[str(self.id)])
 
+    # overwrites a method from models.Model used on create/update
+    def save(self, *args, **kwargs):
+        if not self.pk and not self.hourly_sales:
+            min = self.minimum_customers_per_hour
+            max = self.maximum_customers_per_hour
+
+            cookies_each_hour = [
+                int(random.randint(min, max) * self.average_cookies_per_sale)
+                for _ in range(14)
+            ]
+
+            self.hourly_sales = cookies_each_hour
+
+
+        super().save(*args, **kwargs)
